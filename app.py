b5b0394b33ad4b1ec0a4e8f6e7544aaec8024632
dudeman123 <maxlou188@gmail.com>
2022-02-27 22:43:06 -0600

    authentication

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ from models import db, User, ApiNavigator
 from views import bookmarks, comments, followers, following, \
     posts, profile, stories, suggestions, post_likes
 
-
+import flask_jwt_extended
 
 app = Flask(__name__)
+
+# Initialize JWT convenience library with the special key
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')  # Change this "super secret" with something else!
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
+app.config["JWT_COOKIE_SECURE"] = False
+jwt = flask_jwt_extended.JWTManager(app)
+
 
 # CORS: allows anyone from anywhere to use your API:
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -58,7 +65,23 @@ def api_docs():
         url_root=request.url_root[0:-1] # trim trailing slash
     )
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Check database
+        results = User.query.filter_by(username=username).all()
+        if len(results) == 1:
+            user=results[0]
+            print(user)
+            print('set token')
+        else:
+            return 'invalid'
 
+        print('handle authentication and token setting')
+    return render_template('login.html')
 
 # enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
